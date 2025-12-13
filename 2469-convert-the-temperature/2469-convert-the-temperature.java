39e14534a4f8cc6ac9c1b4031aa61cfdf3e3bc37
JEYAPRIYA2315 <jeyapriyajeyalakshmi@gmail.com>
2025-12-13 15:41:11 +0530

    Time: 0 ms (100%), Space: 45.8 MB (57.63%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double a = celsius + 273.15;
+        double b = celsius * 1.80 + 32.00;
+return new double[]{a,b};
+
+    }
+}
