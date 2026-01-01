7f52110e5b31408bf9ce3b4566d41a30a9d96a11
JEYAPRIYA2315 <jeyapriyajeyalakshmi@gmail.com>
2026-01-01 10:49:31 +0530

    Time: 1 ms (28.99%), Space: 42.4 MB (62.9%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingWeight(int n) {
+        String binary="";
+        int count=0;
+        while(n>0)
+        {
+          if(n%2==1)
+          {
+            count++;
+          }
+          n/=2;
+        }
+          return count;
+        }
+    }
